c205dc26bd87c70269e6ecd4528d30ada17e6af4
Tobiáš Licek <licek@voatt.cz>
2025-09-12 18:52:42 +0200

    Style right guess

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 console.log(secretNumber);
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,6 +13,10 @@ document.querySelector('.check').addEventListener('click', function () {
       'Number has to be bigger than 0';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Congratulations!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').style.fontSize = '80px';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high';
