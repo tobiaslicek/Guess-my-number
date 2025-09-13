5261d4d7d482b1d583a928976a61dfd3bcc23810
Tobiáš Licek <licek@voatt.cz>
2025-09-13 15:04:11 +0200

    refactor: streamline message display logic for better readability

--- script.js
+++ script.js
@@ -2,44 +2,34 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-console.log(secretNumber);
 let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
   if (!guess) {
-    document.querySelector('.message').textContent =
-      'Number has to be bigger than 0';
+    displayMessage('Number has to be bigger than 0');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Congratulations!';
+    displayMessage('Congratulations');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').style.fontSize = '80px';
-
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = score;
-      document.querySelector('.message').textContent =
-        'Congratulations, new highscore!';
+      displayMessage('Congratulations, new highscore!');
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high';
+      displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'Sorry, no more retries';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Sorry, no more retries';
+      displayMessage('Sorry, no more retries');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -48,8 +38,7 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
